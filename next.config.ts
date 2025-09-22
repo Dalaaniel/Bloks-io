9ba6d8d5ff1d0d13d893493dc7a08f758bc6d044
al.cp <alan@gilplanet.net>
2025-09-22 02:44:08 +0200

    vercel switch

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,5 @@
 import type {NextConfig} from 'next';
 
-module.exports = {
-  async headers() {
-    return [
-      {
-        source: '/_next/*',  // Targets Next.js build assets
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'no-store, no-cache, must-revalidate', // Ensures no caching for these files
-          },
-        ],
-      },
-    ]
-  },
-}
-
 
 const nextConfig: NextConfig = {
   /* config options here */
